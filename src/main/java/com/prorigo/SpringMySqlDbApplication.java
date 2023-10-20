0ae08065cef3ebddfd244e6b01de334737e6cb8d
elearning <waghashish007@gmail.com>
2023-10-20 12:53:00 +0530

    first commit

---
+++ src/main/java/com/prorigo/SpringMySqlDbApplication.java
@@ -0,0 +1,13 @@
+package com.prorigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMySqlDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMySqlDbApplication.class, args);
+	}
+
+}
